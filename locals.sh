0d7c544e69c68abf9ecfe42cf22c657f7cffddd6
Ingo Fahrentholz <ingof.work@gmail.com>
2016-01-17 22:32:20 +0100

    Update locals.sh

--- locals.sh
+++ locals.sh
@@ -11,11 +11,30 @@ npm i -D webpack-dev-server
 
 
 echo ""
+echo "========================================="
+echo "=         Install test modules          ="
+echo "-----------------------------------------"
+echo "* karma"
+echo "* karma-jasmine"
+echo "* karma-chrome-launcer"
+echo "========================================="
+echo ""
+
+
+npm i -D karma
+npm i -D karma-jasmine 
+npm i -D karma-chrome-launcher
+
+
+echo ""
 echo ""
 echo "========================================="
 echo "$        Finished installation          $"
 echo "-----------------------------------------"
 echo "* webpack"
+echo "* karma"
+echo "* karma-jasmine"
+echo "* karma-chrome-launcer"
 echo "========================================="
 echo ""
 echo ""
